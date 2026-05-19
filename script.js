8d730852e77635c5bfc557ce6a4774abd9ea0aa8
GUTTA.VUNNATHI <vunnathichowdary@gmail.com>
2026-05-19 16:39:06 +0530

    Added intro screen transition functionality

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ setTimeout(function () {
         .classList.remove("hidden");
 
 }, 4000);
+
+
+
+function addTask() {
+
+    let taskInput =
+        document.getElementById("taskInput");
+
+    let taskText =
+        taskInput.value;
+
+    if(taskText === "") {
+
+        alert("Please enter a task");
+
+        return;
+    }
+
+    let li =
+        document.createElement("li");
+
+    li.textContent = taskText;
+
+    document
+        .getElementById("taskList")
+        .appendChild(li);
+
+    taskInput.value = "";
+}
