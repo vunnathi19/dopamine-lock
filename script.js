7f94ee8dbfdd0c5ac5743c83e9969da53a6e965e
GUTTA.VUNNATHI <vunnathichowdary@gmail.com>
2026-05-19 16:42:35 +0530

    updated script

--- script.js
+++ script.js
@@ -27,10 +27,45 @@ function addTask() {
         return;
     }
 
-    let li =
-        document.createElement("li");
+  let li =
+    document.createElement("li");
 
-    li.textContent = taskText;
+
+let checkbox =
+    document.createElement("input");
+
+checkbox.type = "checkbox";
+
+
+let span =
+    document.createElement("span");
+
+span.textContent = taskText;
+
+
+checkbox.addEventListener("change", function () {
+
+    if (checkbox.checked) {
+
+        span.style.textDecoration =
+            "line-through";
+
+        span.style.opacity = "0.6";
+    }
+
+    else {
+
+        span.style.textDecoration =
+            "none";
+
+        span.style.opacity = "1";
+    }
+});
+
+
+li.appendChild(checkbox);
+
+li.appendChild(span);
 
     document
         .getElementById("taskList")
